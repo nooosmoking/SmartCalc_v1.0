45c95db9092097a171861115b8c2c75eff0eb25d
Kaleighh Hyde <kaleighh@student.21-school.ru>
2023-05-03 19:05:17 +0700

    final no build

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kaleighh/C7_SmartCalc_v1.0-2/build/_deps/raylib-src
-# Build directory: /Users/kaleighh/C7_SmartCalc_v1.0-2/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kaleighh/C7_SmartCalc_v1.0-2/build/_deps/raylib-src/src
-# Build directory: /Users/kaleighh/C7_SmartCalc_v1.0-2/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
